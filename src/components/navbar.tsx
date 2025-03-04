69e366ccdd41abc11bcffd1857240adffefc22a0
Arn <arnaud@talentpicker.io>
2025-03-04 15:18:13 +0100

    fix a in a

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,7 +2,6 @@ import { Link, useLocation } from "react-router";
 import {
   NavigationMenu,
   NavigationMenuItem,
-  NavigationMenuLink,
   navigationMenuTriggerStyle,
 } from "./ui/navigation-menu";
 import { ModeToggle } from "./ui/mode-toggle";
@@ -23,9 +22,9 @@ const Navbar = () => {
         {navs.map((nav) => {
           const isActive = location.pathname === nav.to;
           return (
-            <NavigationMenuItem key={nav.text}>
-              <Link to={nav.to}>
-                <NavigationMenuLink
+            <Link to={nav.to} key={nav.text}>
+              <NavigationMenuItem>
+                <span
                   className={cx(
                     navigationMenuTriggerStyle(),
                     isActive &&
@@ -33,9 +32,9 @@ const Navbar = () => {
                   )}
                 >
                   {nav.text}
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
+                </span>
+              </NavigationMenuItem>
+            </Link>
           );
         })}
       </div>
